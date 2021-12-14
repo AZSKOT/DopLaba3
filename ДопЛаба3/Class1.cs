c5ba3b1aa26b5daba48f06557f1cf8fe70887005
AZSKOT <ko.tanchik@inbox.ru>
2021-12-14 16:01:29 +0300

    TestCommit

---
+++ ДопЛаба3/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ДопЛаба3
+{
+    public class RunHooks
+    {
+        public static void RunPreCommit()
+        {
+            Console.WriteLine("WriteLine HOOK!!!! HOOK!!!");
+        }
+    }
+}
