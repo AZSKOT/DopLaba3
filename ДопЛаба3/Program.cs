4541376cf2547f79ccfc6518181b77ce2d1bbc88
AZSKOT <ko.tanchik@inbox.ru>
2021-12-14 15:00:41 +0300

    TestCommit

---
+++ ДопЛаба3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ДопЛаба3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
